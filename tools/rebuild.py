dc3b0875e778a8b6d44d627f20de07261a1de985
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2008-05-17 00:26:20 -0700

    basic rebuild.py working; write some content

--- tools/rebuild.py
+++ tools/rebuild.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-import os.path
+import os.path, sys
+import textile
 
 CURDIR = os.path.curdir
 SRCDIR = os.path.realpath(os.path.join(CURDIR, "txtsrc"))
@@ -12,22 +13,51 @@ def verify_dir_exists(path):
         print("dir '%s' doesn't exists" % path)
         sys.exit(1)
 
+def ensure_dir(path):
+    if not dir_exists(path):
+        os.mkdir(path)
+
+VALID_SUFFS = (".txt", ".textile")
 def issrcfile(path):
-    return path.endswith(".txt")
+    for suff in VALID_SUFFS:
+        if path.endswith(suff):
+            return True
+    return False
 
 def outfilename(path):
     base = os.path.basename(path)
-    base = base.replace(".txt", ".html")
-    return os.path.join(OUTDIR, base)
+    print base
+    for suff in VALID_SUFFS:
+        if base.endswith(suff):
+            base = base.replace(suff, ".html")
+            return os.path.join(OUTDIR, base)
+    return None
+
+def read(path):
+    fo = open(path, "rb")
+    d = fo.read()
+    fo.close()
+    return d
+
+def write(path, data):
+    fo = open(path, "wb")
+    fo.write(data)
+    fo.close()
 
 def dofile(srcpath):
     if not issrcfile(srcpath):
         print("Skipping '%s'" % srcpath)
         return
     dstpath = outfilename(srcpath)
-    print(dstpath)
+    if dstpath is None:
+        print("Ignoring file '%s'" % srcpath)
+        return
+    txt = read(srcpath)
+    html = textile.textile(txt)
+    write(dstpath, html)
 
 def main():
+    ensure_dir(OUTDIR)
     files = [os.path.join(SRCDIR, f) for f in os.listdir(SRCDIR)]
     map(dofile, [f for f in files if os.path.isfile(f)])
 
---
+++ txtsrc/ParseString.h
@@ -0,0 +1,12 @@
+#ifndef PARSED_STR_H__
+#define PARSED_STR_H__
+
+class ParsedStr {
+public:
+    ParsedStr(const char* src);
+    size_t count();
+    const char* name(size_t idx);
+    const char* value(size_t idx);
+};
+
+#endif
